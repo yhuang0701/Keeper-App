999dfff083f0e5ecab2656ce590b8edc8fe2282d
Yichen Huang <achen765223419@gmail.com>
2022-06-21 21:56:51 -0700

    Update Notes Component & Add Render Examples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import NoteItems from './Notes';
 
 function App() {
     return (
         <>
             <Header />
-            <Note />
+
+            {NoteItems.map(noteItem => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))};
+
+
             <Footer />
         </>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-function Note() {
+function Note(prop) {
     return (
-        <div className="note">
-            <h1>This is the node title</h1>
-            <p>This is the node content</p>
+        <div className='note'>
+            <h1>{prop.title}</h1>
+            <p>{prop.content}</p>
         </div>
     )
 };
